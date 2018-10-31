152b5235a3e547d9d771ae36cd9433339bc97e77
Alejo Muñoz <chalo991220@gmail.com>
2018-10-31 10:28:22 -0500

    three commit

--- preIser/settings.py
+++ preIser/settings.py
@@ -133,10 +133,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = 'media'
 MEDIA_URL = '/media/'
 
-MIDDLEWARE = [
+#MIDDLEWARE = [
   # 'django.middleware.security.SecurityMiddleware',
-  'whitenoise.middleware.WhiteNoiseMiddleware',
+#  'whitenoise.middleware.WhiteNoiseMiddleware',
   # ...
-]
+#]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
